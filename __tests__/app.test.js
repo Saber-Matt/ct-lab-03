50e74207fac837b39658702e23d9455762369449
Matthew Saber <mattsaber21@yahoo.com>
2021-06-21 10:13:36 -0700

    code chunk

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,29 +1,43 @@
-// const pool = require('../lib/utils/pool');
-// const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const Order = require('../lib/models/Order')
 
-// jest.mock('twilio', () => () => ({
-//   messages: {
-//     create: jest.fn(),
-//   },
-// }));
+jest.mock('twilio', () => () => ({
+  messages: {
+    create: jest.fn(),
+  },
+}));
 
-// describe('03_separation-of-concerns-demo routes', () => {
-//   beforeEach(() => {
-//     return setup(pool);
-//   });
+describe('07 demo routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
 
-//   it('creates a new order in our database and sends a text message', async () => {
-//     const expectation = {
-//       id: '1',
-//       quantity: 10,
-//     };
+  it('creates a new order in our database and sends a text message', async () => {
+    const res = await request(app)
+      .post('/api/v1/orders')
+      .send({ 
+        item: 'infinity stone',
+        quantity: 1
+      });
+      expect(res.body).toEqual({
+        id: '1',
+        item: 'infinity stone',
+        quantity: 1
+      });
+    });
 
-//     const res = await request(app)
-//       .post('/api/v1/orders')
-//       .send({ quantity: 10 });
-
-//     expect(res.body).toEqual(expectation);
-//   });
-// });
+    it('finds order in database', async() => {
+      const order = await Order.insert({
+        item: 'tupperware',
+        quantity: 3
+      });
+  
+      const res = await request(app)
+        .get(`/api/v1/orders/${order.id}`);
+  
+      expect(res.body).toEqual(order);
+    });
+  });
---
+++ server.js
@@ -0,0 +1,15 @@
+
+import app from './lib/app.js';
+import pool from './lib/utils/pool.js';
+
+const PORT = process.env.PORT || 7890;
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Started on ${PORT}`);
+});
+
+process.on('exit', () => {
+  console.log('Until we meet again');
+  pool.end();
+});
