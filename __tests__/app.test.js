e704ac3d5a664a34f6ee4a7c1e30c90a71046bc8
Matthew Saber <mattsaber21@yahoo.com>
2021-06-27 17:33:47 -0700

    need help with this error, i think tests should pass afterward

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -18,26 +18,67 @@ describe('07 demo routes', () => {
   it('creates a new order in our database and sends a text message', async () => {
     const res = await request(app)
       .post('/api/v1/orders')
-      .send({ 
+      .send({
         item: 'infinity stone',
         quantity: 1
       });
-      expect(res.body).toEqual({
-        id: '1',
-        item: 'infinity stone',
-        quantity: 1
-      });
-    });
-
-    it('finds order in database', async() => {
-      const order = await Order.insert({
-        item: 'tupperware',
-        quantity: 3
-      });
-  
-      const res = await request(app)
-        .get(`/api/v1/orders/${order.id}`);
-  
-      expect(res.body).toEqual(order);
+    expect(res.body).toEqual({
+      id: '1',
+      item: 'infinity stone',
+      quantity: 1
     });
   });
+
+  it('finds order in database by Id', async () => {
+    const order = await Order.insert({
+      item: 'tupperware',
+      quantity: 3
+    });
+
+    const res = await request(app)
+      .get(`/api/v1/orders/${order.id}`);
+
+    expect(res.body).toEqual(order);
+  });
+
+  it('Deletes an order by id via', async () => {
+    const order = await Order.insert({
+      quantity: 10
+    });
+
+    const res = await request(app).delete(`/api/v1/orders/${order.id}`);
+
+    expect(res.body).toEqual(order);
+  });
+
+  it('gets all orders', async () => {
+    const order1 = await Order.insert({
+      quantity: 7
+    });
+
+    const order2 = await Order.insert({
+      quantity: 2
+    });
+
+    const order3 = await Order.insert({
+      quantity: 14
+    });
+
+    const res = await request(app).get('/api/v1/orders');
+    expect(res.body).toEqual([order1, order2, order3]);
+  });
+
+  it('Updates an order', async () => {
+    const order = await Order.insert({
+      quantity: 4
+    });
+
+    order.quantity = 4;
+
+    const res = await request(app).put(`/api/v1/orders/${order.id}`)
+      .send(order);
+    expect(res.body).toEqual(order);
+  });
+
+
+});
